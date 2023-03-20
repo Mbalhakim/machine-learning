d9058d38e35f7a69f660ca6b0852f8a26e98ddde
Mbalhakim <mb.alhakim@gmail.com>
2023-03-20 17:34:15 +0100

    added main.py and the data set

---
+++ main.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+cols = ["fLength", "fWidth", "fSize", "fConc", "fConc1", "fAsym", "fM3Long", "fM3Trans", "fAlpha", "fDist", "class"]
+
+df = pd.read_csv("magic04.data", names=cols)
+
+df["class"] = (df["class"] == "g").astype(int)
+# pd.set_option('display.max_columns', None)
+print(df.head())
